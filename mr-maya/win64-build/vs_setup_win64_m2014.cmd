25ba6a4f6937e1cd2b18c725d763431ffbe02231
HtwoO <hxf.prc+web@gmail.com>
2015-11-18 10:24:51 +0800

    WinSDK 7.1 installation failure fix
    Windows SDK 7.1 installation failure bug and fix link

--- mr-maya/win64-build/vs_setup_win64_m2014.cmd
+++ mr-maya/win64-build/vs_setup_win64_m2014.cmd
@@ -9,10 +9,14 @@ set INCLUDE=%INCLUDE%;C:\Program Files\Autodesk\mentalrayForMaya2014\devkit\incl
 :: Setup VC for 64 bit compiling
 REM ~ vcvarsall.bat x64
 
-:: https://stackoverflow.com/questions/1865069/how-to-compile-a-64-bit-application-using-visual-c-2010-express
+
 :: According to https://stackoverflow.com/questions/5128969/how-do-i-load-the-64-bit-visual-studio-tools-command-prompt ,
 :: due to a [bug(648412)](https://connect.microsoft.com/VisualStudio/feedback/details/648412/) in vs2010 express,
 :: Visual Studio 2010 Express is installed partially, therefore vcvarsall.bat is missing.
-:: By installing win sdk 7.1, and run the following command, vs should be properly configured.
+
+:: If Windows SDK 7.1 failed to install, it may due to: https://support.microsoft.com/kb/2717426
+:: Visual C++ compilers are removed when you upgrade Visual Studio 2010 Professional or Visual Studio 2010 Express to Visual Studio 2010 SP1 if Windows SDK v7.1 is installed. https://support.microsoft.com/kb/2519277
+
+:: After installing win sdk 7.1, run the following command, VS should be properly configured.
 CALL "C:\Program Files\Microsoft SDKs\Windows\v7.1\bin\SetEnv.Cmd" /x64 /Release
 REM ~ CALL SetEnv /x64 /Debug
