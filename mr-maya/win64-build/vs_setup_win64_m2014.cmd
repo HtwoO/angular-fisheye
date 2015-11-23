eb9dddaf6f82f4afc7d0518e6f20c449699051ba
Fonzie Huang <hxf.prc+web@gmail.com>
2015-11-23 23:37:59 +0800

    working vs 2010 express 64 bit compile setup

--- mr-maya/win64-build/vs_setup_win64_m2014.cmd
+++ mr-maya/win64-build/vs_setup_win64_m2014.cmd
@@ -1,25 +1,23 @@
-REM ~ https://social.msdn.microsoft.com/Forums/en-US/e0ef4613-d90f-4eec-90db-41339ed31367/vs2010-express-and-missing-x64-compiler?forum=Vsexpressinstall
+:: Setup Visual Studio full version for 64 bit compiling
+REM vcvarsall.bat x64
 
-ECHO. mental ray shader building env setup script
-PATH=C:\Program Files (x86)\Microsoft Visual Studio 10.0\Common7\IDE\;C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\BIN;C:\Program Files (x86)\Microsoft Visual Studio 10.0\Common7\Tools;C:\Windows\Microsoft.NET\Framework\v4.0.30319;C:\Windows\Microsoft.NET\Framework\v3.5;C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\VCPackages;C:\Program Files (x86)\Microsoft SDKs\Windows\v7.1\bin\NETFX 4.0 Tools;C:\Program Files (x86)\Microsoft SDKs\Windows\v7.1\bin;C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\;%PATH%
+:: Setup VS 2010 Express version for 64 bit compiling
+:: VS 2010 Express is installed partially, therefore vcvarsall.bat is missing.
 
-:: Add mental ray include directory to %INCLUDE%
-set INCLUDE=%INCLUDE%;C:\Program Files\Autodesk\mentalrayForMaya2014\devkit\include\
+:: If Windows SDK 7.1 failed to install, it may due to newer version of
+:: vc++ redistributable or runtime presents in the system then Windows 7.1 SDK
+:: one. See https://support.microsoft.com/kb/2717426
 
-:: Setup VC for 64 bit compiling
-REM ~ vcvarsall.bat x64
-
-
-:: According to https://stackoverflow.com/questions/5128969/how-do-i-load-the-64-bit-visual-studio-tools-command-prompt ,
-:: due to a [bug(648412)](https://connect.microsoft.com/VisualStudio/feedback/details/648412/) in vs2010 express,
-:: Visual Studio 2010 Express is installed partially, therefore vcvarsall.bat is missing.
-
-:: https://www.mathworks.com/matlabcentral/answers/95039-why-does-the-sdk-7-1-installation-fail-with-an-installation-failed-message-on-my-windows-system
-:: If Windows SDK 7.1 failed to install, it may due to: https://support.microsoft.com/kb/2717426
-:: Visual C++ compilers are removed when you upgrade Visual Studio 2010 Professional or Visual Studio
-:: 2010 Express to Visual Studio 2010 SP1 if Windows SDK v7.1 is installed.
+:: Visual C++ compilers are removed when you upgrade VS 2010 Professional or
+:: VS 2010 Express to VS 2010 SP1 if Windows SDK v7.1 is installed.
 :: https://support.microsoft.com/kb/2519277
 
-:: After installing win sdk 7.1, run the following command, VS should be properly configured.
+:: After installing win sdk 7.1, run the following command,
+:: VS should be properly configured.
 CALL "C:\Program Files\Microsoft SDKs\Windows\v7.1\bin\SetEnv.Cmd" /x64 /Release
-REM ~ CALL SetEnv /x64 /Debug
+REM CALL SetEnv /x64 /Debug
+
+ECHO. mental ray shader building env setup script
+:: Add mental ray include directory to %INCLUDE% path
+set INCLUDE=%INCLUDE%;C:\Program Files\Autodesk\mentalrayForMaya2014\devkit\include\
+
